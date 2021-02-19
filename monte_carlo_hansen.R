e2fd38fab6fad35204e29ce167911bd85b41f5b9
Ashton <ashhansen6@outlook.com>
2021-02-19 14:15:36 -0600

    Adjusted sim function to output cumulative return (USD); added visual of simulation for TLT

--- monte_carlo_hansen.R
+++ monte_carlo_hansen.R
@@ -26,6 +26,7 @@ prices <-
 
 prices <- data.frame(prices)
 prices_sub <- prices[,!sapply(prices, function(x) any(is.na(x)))]
+colnames(symbols_updated) <- colnames(prices_sub)
 
 # Calculate non-transformed returns (dollar amount)
 returns <- data.frame()
@@ -34,7 +35,8 @@ for (j in 1:ncol(prices_sub)){
     returns[i,j] <- prices_sub[i+1,j] - prices_sub[i,j]
   }
 }
-colnames(returns) <- colnames(prices_sub)
+colnames(returns) <- colnames(symbols_updated)
+
 
 # Cleaning up i and j for later
 rm(i)
@@ -55,11 +57,22 @@ rm(j)
 ###   nsim = number of simulations
 ## *Can use with mapply() for different distributions, periods, etc.* 
 
-mc.sim <- function(x, dist = "normal", period_type = 'daily', period_count = "365", nsim = 1000){
+# Making distribution vector for each security
+distribution <- c()
+
+
+mc.sim <- function(x, 
+                   dist = "normal", 
+                   period_type = 'daily', 
+                   period_count = "365",
+                   init_invest = 0,
+                   nsim = 1000){
   cumulative <- data.frame()
   mean <- c()
   stdev <- c()
   sim_return <- matrix(nrow = strtoi(period_count, 10L), ncol = NCOL(x))
+  sim_accum <- matrix(nrow = strtoi(period_count, 10L), ncol = NCOL(x))
+  sim_accum[1,] <- init_invest
   sim_out <- list()
   for (m in 1:nsim){
     for (j in 1:ncol(x)){
@@ -70,18 +83,40 @@ mc.sim <- function(x, dist = "normal", period_type = 'daily', period_count = "36
           sim_return[,j] <- rnorm(strtoi(period_count, 10L),
                                   mean[j],
                                   stdev[j])
-        }
+          for (i in 2:nrow(sim_return)){
+          sim_accum[i,j] <- sim_accum[i-1,j] + sim_return[i,j]
+          }
+            }
       }
     }
-    colnames(sim_return) <- colnames(x)
-    sim_out[[m]] <- sim_return
+    colnames(sim_accum) <- colnames(x)
+    sim_out[[m]] <- sim_accum
   }
   return(sim_out)
 }
 
+# One year of daily returns example
+one_year <- mc.sim(returns)
 
+# Narrowing example down to TLT
+TLT_sims <- matrix(nrow = 365, ncol = length(one_year))
+for (s in 1:length(one_year)){
+  TLT_sims[,s] <- unlist(one_year[[s]][,"TLT"])
+}
 
+# Plotting Accumulated Returns
+# MAY TAKE A LONG TIME (< 30 minutes), DO NOT RUN IF NOT NECESSARY!
+TLT_sims <- data.frame(TLT_sims)
+index = seq(1,365,1)
 
-
-
-
+set.seed(1456)
+plot(TLT_sims$X1 ~ index,
+     type = "l",
+     main = "TLT Cumulative Return Simulation",
+     ylab = "Cumulative Return (USD)",
+     ylim = c(-75, 75),
+     xlab = "Day",
+     col = "red")
+for (p in 2:length(TLT_sims)){
+  lines(TLT_sims[,p] ~ index, col = sample(colors(), replace = F))
+}
