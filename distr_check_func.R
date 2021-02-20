4ed03f2267f222141ad25759e2cbfe0e04263015
Ashton <ashhansen6@outlook.com>
2021-02-20 12:31:02 -0600

    Added a script exclusively for an optional input screening function

---
+++ distr_check_func.R
@@ -0,0 +1,13 @@
+# As the correct mean, StDev, and sampling distribution inputs are required 
+# for an accurate MC Simulation, this function is intended to test which 
+# sampling distribution would be best for the given security.
+
+# *As always, one can use one of the apply() functions to use this function
+#  with a vector.
+
+mc.distr(x){
+  distribution <- c()
+  # Normal Distribution
+  
+  
+}
