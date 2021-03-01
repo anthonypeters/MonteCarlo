33a7caba5702a1da720133d11c7df2e3cfaaed97
Karthik Myneni <2kpmyneni@gmail.com>
2021-03-01 17:28:32 -0600

    Hello

--- monteCarloMain.R
+++ monteCarloMain.R
@@ -10,3 +10,4 @@ quandl_api_key("mRJDZwn3giwAm1kowtFr")
 symbols <- readxl::read_excel("tickers.xlsx", sheet = "Tickers")
 modified_tickers <- sprintf("EOD/%s", symbols$Ticker)
 stock_prices <- tq_get(modified_tickers, get = "quandl", from = Sys.Date()-(365*13), to = Sys.Date()-(365*3))
+## Hello 
